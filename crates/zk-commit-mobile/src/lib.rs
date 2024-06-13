0af3eb91f3780a8257f9c2992f8434360d2afd49
jingyi.zhao <jingyi.zhao@okg.com>
2024-06-13 19:12:36 +0800

    Add Mobile POC Code

---
+++ crates/zk-commit-mobile/src/lib.rs
@@ -0,0 +1,33 @@
+use std::time::{Duration, Instant};
+use zk_commit_core::fibonacci;
+
+uniffi::setup_scaffolding!();
+
+#[derive(uniffi::Error, Debug, thiserror::Error)]
+pub enum ProveError {
+    #[error("Failed to execute Fibonacci Proof")]
+    FibonacciError,
+}
+
+
+#[derive(uniffi::Record)]
+pub struct ProofResult {
+    pub first_input: u64,
+    pub second_input: u64,
+    pub output: u64,
+    pub duration: Duration,
+}
+
+#[uniffi::export]
+pub fn fibonacci_mobile() -> Result<ProofResult, ProveError> {
+    let start = Instant::now();
+    match fibonacci() {
+        Ok(result) => Ok(ProofResult {
+            first_input: result.input.0.0,
+            second_input: result.input.1.0,
+            output: result.output.0,
+            duration: start.elapsed(),
+        }),
+        Err(_) => Err(ProveError::FibonacciError),
+    }
+}
