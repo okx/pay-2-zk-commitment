fbc8a5ced9e35a2b4a3a6c9e511dc11808510a3e
cliff0412 <cliff.yang@okg.com>
2024-06-25 17:38:08 +0800

    use cbn128 config

--- crates/zk-commit-core/src/prover.rs
+++ crates/zk-commit-core/src/prover.rs
@@ -1,11 +1,19 @@
 use std::fs::File;
 
-use anyhow::{Result,anyhow};
+use anyhow::{anyhow, Result};
 use log::Level;
 use plonky2::{
-    iop::witness::PartialWitness,
+    field::extension::Extendable,
+    gates::noop::NoopGate,
+    hash::{hash_types::RichField, poseidon_bn128::PoseidonBN128Hash},
+    iop::witness::{PartialWitness, WitnessWrite},
     plonk::{
-        circuit_builder::CircuitBuilder, circuit_data::{CircuitData, CommonCircuitData, VerifierOnlyCircuitData}, proof::ProofWithPublicInputs,
+        circuit_builder::CircuitBuilder,
+        circuit_data::{
+            CircuitConfig, CircuitData, CommonCircuitData, VerifierCircuitTarget, VerifierOnlyCircuitData
+        },
+        config::{AlgebraicHasher, GenericConfig, GenericHashOut, Hasher},
+        proof::{ProofWithPublicInputs, ProofWithPublicInputsTarget},
         prover::prove,
     },
     util::timing::TimingTree,
@@ -19,7 +27,7 @@ use crate::{
     claim_circuit::{generate_claim_circuit, set_claim_circuit},
     claim_execution::{get_claim_proving_inputs, Claim},
     commitment_tree::CommitmentTree,
-    types::{C, F},
+    types::{Cbn128, C, F},
     utils::AmountSecretPairing,
 };
 
@@ -37,7 +45,8 @@ pub fn generate_proof_of_claim(
     index: usize,
     commitment_tree: CommitmentTree,
     path: &str,
-) -> Result<(ProofWithPublicInputs<F, C, D>, VerifierOnlyCircuitData<C, D>, CommonCircuitData<F, D>)> {
+) -> Result<(ProofWithPublicInputs<F, Cbn128, D>, VerifierOnlyCircuitData<Cbn128, D>, CommonCircuitData<F, D>)>
+{
     // Create claim from inputs
     let claim = Claim {
         pair: AmountSecretPairing { amount, secret },
@@ -59,7 +68,7 @@ pub fn generate_proof_of_claim(
     // Build circuit from data and prove
     builder.print_gate_counts(0);
     let mut timing = TimingTree::new("prove", Level::Debug);
-    let data = builder.build::<C>();
+    let data = builder.build::<Cbn128>();
     let CircuitData { prover_only, common, verifier_only: _ } = &data;
     let proof_res = prove(&prover_only, &common, pw, &mut timing);
 
@@ -90,6 +99,107 @@ pub fn generate_proof_of_claim(
 
     // return Result::Ok(());
 }
+
+// pub fn recursive_single_proof<
+//     F: RichField + Extendable<D>,
+//     C: GenericConfig<D, F = F>,
+//     InnerC: GenericConfig<D, F = F>,
+//     const D: usize,
+// >(
+//     inner_proof: &ProofWithPublicInputs<F, InnerC, D>,
+//     inner_vd: &VerifierOnlyCircuitData<InnerC, D>,
+//     inner_cd: &CommonCircuitData<F, D>,
+//     config: &CircuitConfig,
+//     min_degree_bits: Option<usize>,
+//     print_gate_counts: bool,
+//     is_bn254: bool,
+// ) -> Result<(ProofWithPublicInputs<F, C, D>, VerifierOnlyCircuitData<C, D>, CommonCircuitData<F, D>)>
+// where
+//     InnerC::Hasher: AlgebraicHasher<F>,
+//     [(); C::Hasher::HASH_SIZE]:,
+// {
+//     let now = std::time::Instant::now();
+
+//     let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+//     let mut pw = PartialWitness::new();
+//     let pt: ProofWithPublicInputsTarget<D> =
+//         builder.add_virtual_proof_with_pis::<InnerC>(&inner_cd);
+//     pw.set_proof_with_pis_target(&pt, &inner_proof);
+
+//     let inner_data = VerifierCircuitTarget {
+//         constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
+//         circuit_digest: builder.add_virtual_hash(),
+//     };
+//     pw.set_cap_target(&inner_data.constants_sigmas_cap, &inner_vd.constants_sigmas_cap);
+
+
+
+//     // Register all public inputs
+//     println!(
+//         "register public inputs: {:?}, size: {:?}",
+//         pt.public_inputs.as_slice(),
+//         pt.public_inputs.as_slice().len()
+//     );
+//     builder.register_public_inputs(pt.public_inputs.as_slice());
+
+//     // we do not need to register circuit digest on last recursive layer; as it is hard code as a constant in the circom constant file
+
+//     println!("register circuit_digest: {:?}", inner_data.circuit_digest.elements.as_slice());
+//     builder.register_public_inputs(inner_data.circuit_digest.elements.as_slice());
+
+//     // register vd root as public input
+//     println!(
+//         "register vd_root: {:?}",
+//         vd_proof_verification_targets.vd_root_target.elements.as_slice()
+//     );
+//     builder
+//         .register_public_inputs(vd_proof_verification_targets.vd_root_target.elements.as_slice());
+
+//     if is_bn254 {
+//         let mut configs = vec![];
+//         for i in 0..builder.config.fri_config.num_cap_elements() {
+//             // let sigma_hash = builder.hash_n_to_hash_no_pad(inner_data.constants_sigmas_cap.0[i].elements);
+//             configs.append(&mut inner_data.constants_sigmas_cap.0[i].elements.to_owned().to_vec());
+//         }
+//         let hash = builder.hash_n_to_hash_no_pad::<PoseidonBN128Hash>(configs);
+//         println!("register fri hash: {:?}", hash.elements);
+//         builder.register_public_inputs(&hash.elements);
+//     }
+
+//     builder.verify_proof::<InnerC>(&pt, &inner_data, &inner_cd);
+
+//     if print_gate_counts {
+//         builder.print_gate_counts(0);
+//     }
+
+//     if let Some(min_degree_bits) = min_degree_bits {
+//         // We don't want to pad all the way up to 2^min_degree_bits, as the builder will add a
+//         // few special gates afterward. So just pad to 2^(min_degree_bits - 1) + 1. Then the
+//         // builder will pad to the next power of two, 2^min_degree_bits.
+//         let min_gates = (1 << (min_degree_bits - 1)) + 1;
+//         for _ in builder.num_gates()..min_gates {
+//             builder.add_gate(NoopGate, vec![]);
+//         }
+//     }
+//     println!("number of gates: {:?}", builder.num_gates());
+//     let data = builder.build::<C>();
+
+//     if is_bn254 {
+//         println!("######################### bn254 recursive prove");
+//     }
+
+//     let mut timing = TimingTree::new(
+//         if is_bn254 { "recursive_single_proof_bn254" } else { "recursive_single_proof_gl64" },
+//         Level::Debug,
+//     );
+//     let start = std::time::Instant::now();
+//     let proof = prove(&data.prover_only, &data.common, pw, &mut timing)?;
+//     println!("proof used: {:?}", start.elapsed());
+//     println!("######################### recursive verify #########################");
+//     data.verify(proof.clone())?;
+//     println!("recursive signle proof time: {:?}", now.elapsed());
+//     Ok((proof, data.verifier_only, data.common))
+// }
 
 /// Writes the proof of a claim to a specified path as a binary file
 pub fn write_to_file(path: &str, proof: ProofWithPublicInputs<F, C, D>) -> std::io::Result<()> {
