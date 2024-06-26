bc1bc2078ae82050b6c399a43ebed24cf3648fda
cliff0412 <cliff.yang@okg.com>
2024-06-26 17:10:04 +0800

    remove rpc in blockchain

--- crates/zk-commit-core/src/blockchain.rs
+++ crates/zk-commit-core/src/blockchain.rs
@@ -2,6 +2,7 @@ use alloy::{
     primitives::{Address, Bytes, FixedBytes, U256},
     providers::{ProviderBuilder, RootProvider},
     sol,
+    sol_types::SolCall,
     transports::http::{Client, Http},
 };
 
@@ -34,32 +35,24 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn new(rpc_url: &str, token_addr: &str, pay_commitment_addr: &str) -> Self {
-        // let provider = Arc::new(Provider::<Http>::try_from(rpc_url).unwrap());
-        let rpc_url = rpc_url.parse().unwrap();
-        let provider = ProviderBuilder::new().on_http(rpc_url);
-
-        let pay_commitment =
-            PayCommitment::new(Address::from_str(pay_commitment_addr).unwrap(), provider.clone());
-        let token: TestERC20Instance<Http<Client>, RootProvider<Http<Client>>> =
-            TestERC20::new(Address::from_str(token_addr).unwrap(), provider.clone());
-        Self { pay_commitment, token }
-    }
-
     // get token approval contract call data; to be signed by mobile wallet
-    pub fn approve_token_call_data(&self, spender: &str, amount: u64) -> Bytes {
+    pub fn approve_token_call_data( spender: &str, amount: u64) -> Vec<u8> {
         let spender = Address::from_str(spender).unwrap();
-        let calldata = self.token.approve(spender, U256::from(amount)).calldata().to_owned();
-        calldata
+        let input = TestERC20::approveCall { spender, amount: U256::from(amount) };
+        let data = input.abi_encode();
+        data
     }
 
-    pub fn get_deposit_token_call_data(&self, amount: u64, commitment: FixedBytes<32>) -> Bytes {
-        let calldata =
-            self.pay_commitment.depositERC20(U256::from(amount), commitment).calldata().to_owned();
-        calldata
+    pub fn get_deposit_token_call_data( amount: u64, commitment: FixedBytes<32>) -> Vec<u8> {
+        let call = PayCommitment::depositERC20Call {
+            _amount: U256::from(amount),
+            _commitment: commitment,
+        };
+        let data = call.abi_encode();
+        data
     }
 
-    pub fn get_claim_token_call_data(&self, pwi: &Groth16ProofWithPublicData) -> Bytes {
+    pub fn get_claim_token_call_data( pwi: &Groth16ProofWithPublicData) -> Vec<u8> {
         let pub_signals = pwi
             .public_data
             .iter()
@@ -70,31 +63,44 @@ impl Blockchain {
         for (i, item) in pub_signals.iter().take(PUB_LENGTH).enumerate() {
             pub_signals_array[i] = *item;
         }
-        let calldata = self
-            .pay_commitment
-            .withdraw(
+
+        let input = PayCommitment::withdrawCall {
+            _pA: [
+                U256::from_str_radix(&pwi.proof.pi_a[0], 10).unwrap(),
+                U256::from_str_radix(&pwi.proof.pi_a[1], 10).unwrap(),
+            ],
+            _pB: [
                 [
-                    U256::from_str_radix(&pwi.proof.pi_a[0], 10).unwrap(),
-                    U256::from_str_radix(&pwi.proof.pi_a[1], 10).unwrap(),
+                    U256::from_str_radix(&pwi.proof.pi_b[0][1], 10).unwrap(),
+                    U256::from_str_radix(&pwi.proof.pi_b[0][0], 10).unwrap(),
                 ],
                 [
-                    [
-                        U256::from_str_radix(&pwi.proof.pi_b[0][1], 10).unwrap(),
-                        U256::from_str_radix(&pwi.proof.pi_b[0][0], 10).unwrap(),
-                    ],
-                    [
-                        U256::from_str_radix(&pwi.proof.pi_b[1][1], 10).unwrap(),
-                        U256::from_str_radix(&pwi.proof.pi_b[1][0], 10).unwrap(),
-                    ],
+                    U256::from_str_radix(&pwi.proof.pi_b[1][1], 10).unwrap(),
+                    U256::from_str_radix(&pwi.proof.pi_b[1][0], 10).unwrap(),
                 ],
-                [
-                    U256::from_str_radix(&pwi.proof.pi_c[0], 10).unwrap(),
-                    U256::from_str_radix(&pwi.proof.pi_c[1], 10).unwrap(),
-                ],
-                pub_signals_array,
-            )
-            .calldata()
-            .to_owned();
-        calldata
+            ],
+            _pC: [
+                U256::from_str_radix(&pwi.proof.pi_c[0], 10).unwrap(),
+                U256::from_str_radix(&pwi.proof.pi_c[1], 10).unwrap(),
+            ],
+            _pubSignals: pub_signals_array,
+        };
+        let data = input.abi_encode();
+        data
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+
+    use super::*;
+
+    #[test]
+    fn test_blockchain() {
+        let call_data = Blockchain::approve_token_call_data("0x1234567890123456789012345678901234567890", 100);
+        let out = hex::encode(call_data);
+        assert_eq!(out, "095ea7b300000000000000000000000012345678901234567890123456789012345678900000000000000000000000000000000000000000000000000000000000000064");
+
     }
 }
